68e64d3d80d05ddef10310e9f288b250b7ac94be
huxw <huxw@mail.jj.cn>
2022-08-10 15:47:54 +0800

    1. 隐藏类哦

---
+++ 2. Fast Properties/util.js
@@ -0,0 +1,30 @@
+
+function setProperties(obj, propertyCount, elementCount) {
+    //添加常规属性
+    for (let i = 0; i < propertyCount; i++) {
+        let ppt = `property${i}`;
+        obj[ppt] = ppt;
+    }
+    // 排序属性
+    for (let i = 0; i < elementCount; i++) {
+        obj[i] = `element${i}`;
+    }
+}
+
+function setProperty(obj, key, value) {
+    obj[key] = value;
+}
+
+
+function addEvent(node, event, listener, options) {
+    node.addEventListener(event, listener, options)
+}
+
+
+function isObject(obj) {
+    return typeof obj === 'object' && obj !== null;
+}
+
+function toString(data) {
+    return isObject(data) ? JSON.stringify(data) : `${data}`
+}
